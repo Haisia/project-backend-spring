eb8efa62c8f66ebad7e31e997077d74580b3d505
Haisia <cwnsgur13579@gmail.com>
2025-05-10 20:02:52 +0900

    수정: NoResourceFoundException 핸들링

--- src/main/java/com/haisia/backend/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/haisia/backend/common/handler/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.haisia.backend.common.handler;
 import com.haisia.backend.common.dto.ResponseData;
 import com.haisia.backend.common.enums.ResponseCode;
 import com.haisia.backend.common.exception.ApplicationException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,14 +29,6 @@ public class GlobalExceptionHandler {
       .body(ResponseData.fail(exception.getCode(), exception.getMessage()));
   }
 
-  @ExceptionHandler(RuntimeException.class)
-  public ResponseEntity<ResponseData<Object>> handleRuntimeException(RuntimeException exception) {
-    log.error("[RuntimeException] : ", exception);
-    return ResponseEntity
-      .status(ResponseCode.INTERNAL_SERVER_ERROR.getHttpStatus())
-      .body(ResponseData.fail(ResponseCode.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."));
-  }
-
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<ResponseData<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
     log.warn("[ValidationException] : {}", exception.getMessage());
@@ -50,6 +44,30 @@ public class GlobalExceptionHandler {
     return ResponseEntity
       .status(ResponseCode.VALIDATION_ERROR.getHttpStatus())
       .body(ResponseData.fail(ResponseCode.VALIDATION_ERROR, combinedMessage));
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<ResponseData<Object>> handleNoResourceFoundException(
+    NoResourceFoundException exception,
+    HttpServletRequest request
+  ) {
+    String requestUrl = request.getRequestURI();
+    String clientIp = request.getRemoteAddr();
+
+    log.warn("[NoResourceFoundException] 요청 URL: {}, 요청 IP: {}, 메시지: {}",
+      requestUrl, clientIp, exception.getMessage());
+
+    return ResponseEntity
+      .status(ResponseCode.RESOURCE_NOT_FOUND_ERROR.getHttpStatus())
+      .body(ResponseData.fail(ResponseCode.RESOURCE_NOT_FOUND_ERROR, ResponseCode.RESOURCE_NOT_FOUND_ERROR.getMessage()));
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ResponseData<Object>> handleRuntimeException(RuntimeException exception) {
+    log.error("[RuntimeException] : ", exception);
+    return ResponseEntity
+      .status(ResponseCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+      .body(ResponseData.fail(ResponseCode.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."));
   }
 
   @ExceptionHandler(Exception.class)
